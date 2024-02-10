dfd32c96c05d91b652031bacfe8da55de1596a3c
CaveNJ <nickallanj3@gmail.com>
2024-02-10 17:57:11 -0500

    Add recipes, add ceramic assets

---
+++ src/main/java/com/redstoneguy10ls/lithiccoins/datagen/ModRecipeProvider.java
@@ -0,0 +1,21 @@
+package com.redstoneguy10ls.lithiccoins.datagen;
+
+import java.util.function.Consumer;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+
+public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
+    public ModRecipeProvider(PackOutput pOutput) {
+        super(pOutput);
+    }
+
+    @Override
+    protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buildRecipes'");
+    }
+
+}

3b9c01a47604402371fc287b5a07782451f6882d
JotredDev <148350408+JotredDev@users.noreply.github.com>
2026-01-10 21:58:54 +0100

    Implemented Firmalife related datagen

--- src/main/java/com/redstoneguy10ls/lithiccoins/datagen/LCMetal.java
+++ src/main/java/com/redstoneguy10ls/lithiccoins/datagen/LCMetal.java
@@ -26,7 +26,7 @@ public enum LCMetal
     BRASS(CoinMaterial.BRASS, Metal.BRASS, 930f, 2),
     BRONZE(CoinMaterial.BRONZE, Metal.BRONZE, 950f, 2),
     CAST_IRON(CoinMaterial.CAST_IRON, Metal.CAST_IRON, 1535f, 1),
-    CHROMIUM(CoinMaterial.CHROMIUM, new MetalType("chromium", "firmalife", 0f, 1)), //TODO: Temperature
+    CHROMIUM(CoinMaterial.CHROMIUM, new MetalType("chromium", "firmalife", 1250f, 4)),
     CONSTANTAN(CoinMaterial.CONSTANTAN, new MetalType("electrum", "tfc_ie_addon", 1250f, 2)),
     COPPER(CoinMaterial.COPPER, Metal.COPPER, 1080f, 1),
     ELECTRUM(CoinMaterial.ELECTRUM, new MetalType("electrum", "tfc_ie_addon", 1000f, 3)),
@@ -36,7 +36,7 @@ public enum LCMetal
     RED_STEEL(CoinMaterial.RED_STEEL, Metal.RED_STEEL, 1540f, 6),
     ROSE_GOLD(CoinMaterial.ROSE_GOLD, Metal.ROSE_GOLD, 960f, 1),
     SILVER(CoinMaterial.SILVER, Metal.SILVER, 961f, 1),
-    STAINLESS_STEEL(CoinMaterial.STAINLESS_STEEL, new MetalType("stainless_steel", "firmalife", 0f, 4)), //TODO: Temperature
+    STAINLESS_STEEL(CoinMaterial.STAINLESS_STEEL, new MetalType("stainless_steel", "firmalife", 1540f, 4)),
     STEEL(CoinMaterial.STEEL, Metal.STEEL, 1540f, 4),
     STERLING_SILVER(CoinMaterial.STERLING_SILVER, Metal.STERLING_SILVER, 950f, 1),
     TIN(CoinMaterial.TIN, Metal.TIN, 230f, 1),

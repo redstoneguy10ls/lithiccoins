f90c58ba17eb39b2288e51f3a028cb7208fd0b4d
JotredDev <148350408+JotredDev@users.noreply.github.com>
2026-05-07 01:09:46 +0200

    Fixed missing sounds for both the coin purse and the mint

--- src/main/java/com/redstoneguy10ls/lithiccoins/common/misc/LCSounds.java
+++ src/main/java/com/redstoneguy10ls/lithiccoins/common/misc/LCSounds.java
@@ -1,8 +1,8 @@
 package com.redstoneguy10ls.lithiccoins.common.misc;
 
-import net.dries007.tfc.util.Helpers;
 import net.dries007.tfc.util.registry.RegistryHolder;
 
+import com.redstoneguy10ls.lithiccoins.util.LCHelpers;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.sounds.SoundEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -15,17 +15,18 @@ public class LCSounds
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(Registries.SOUND_EVENT, MOD_ID);
 
 
-    public static final Id COINPURSE_ADD = register("item.coinpurse.some_add1");
+    public static final Id COINPURSE_ADD_FEW = register("item.coin_purse.add_few");
+    public static final Id COINPURSE_ADD_SOME = register("item.coin_purse.add_some");
+    public static final Id COINPURSE_ADD_MANY = register("item.coin_purse.add_many");
+    public static final Id COINPURSE_REMOVE = register("item.coin_purse.remove");
 
-    public static final Id COINPURSE_EMPTY_ADD = register("item.coinpurse.empty_add");
-
-    public static final Id MINT_HIT = register("block.minting_die.hit");
+    public static final Id MINT_HIT = register("block.mint.hit");
 
 
 
     private static Id register(String name)
     {
-        return new Id(SOUNDS.register(name, () -> SoundEvent.createVariableRangeEvent(Helpers.identifier(name))));
+        return new Id(SOUNDS.register(name, () -> SoundEvent.createVariableRangeEvent(LCHelpers.identifier(name))));
     }
 
     public record Id(DeferredHolder<SoundEvent, SoundEvent> holder) implements RegistryHolder<SoundEvent, SoundEvent> {}
